013708b4da901a8efbb68b233c9f9491afd6ea1d
Shirish Bashyal <shirisjbasjyal@gmail.com>
2026-04-07 10:02:40 +0300

    Delete module3 directory

--- module3/t1/t1.js
+++
@@ -1,7 +0,0 @@
-const listElemet = document.getElementById("target");
-
-addingElement = `<li>First item</li>
-        <li>Second item</li>
-        <li>Third item</li>`
-
-listElemet.innerHTML = addingElement
