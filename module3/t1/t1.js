92144012a6ff57c05077bc71e225cc92204a9e16
Nishan-cmd <nishanKhatiwoda19@gmail.com>
2026-04-07 09:59:24 +0300

    Module 3

---
+++ module3/t1/t1.js
@@ -0,0 +1,7 @@
+const listElemet = document.getElementById("target");
+
+addingElement = `<li>First item</li>
+        <li>Second item</li>
+        <li>Third item</li>`
+
+listElemet.innerHTML = addingElement
